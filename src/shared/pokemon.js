cac59402d1433d123272363d2e1c63e8fbcdf5a7
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-08-31 04:47:21 -0600

    pokemons-header-grandients-styled

--- src/shared/pokemon.js
+++ src/shared/pokemon.js
@@ -1,11 +1,43 @@
 const bgStylePokemonType = {
   grass: "bg-gradient-to-b from-green-500 to-green-300",
   fire: "bg-gradient-to-b from-orange-500 to-orange-300",
+  water: 'bg-gradient-to-b from-blue-500 to-blue-300',
+  rock: 'bg-gradient-to-b from-[#7E7E7E] to-[#8D8D94]',
+  ghost: 'bg-gradient-to-b from-[#323569] to-[#454AA8]',
+  ice: 'bg-gradient-to-b from-[#6FBEDF] to-[#64CBF5]',
+  dark: 'bg-gradient-to-b from-[#0D1211] to-[#5A5E5D]',
+  fairy: 'bg-gradient-to-b from-[#971B45] to-[#CD7D98]',
+  electric: 'bg-gradient-to-b from-[#0C1395] to-[#7075D9]',
+  dragon: 'bg-gradient-to-b from-[#478A93] to-[#A2BEC1]',
+  normal: 'bg-gradient-to-b from-[#735259] to-[#7C3F4C]',
+  fighting: 'bg-gradient-to-b from-[#96402A] to-[#CB735D]',
+  bug: 'bg-gradient-to-b from-[#62DB60] to-[#AAFFA8]',
+  steel: 'bg-gradient-to-b from-[#5E736C] to-[#5E736C]',
+  ground: 'bg-gradient-to-b from-[#654008] to-[#D69638]',
+  poison: 'bg-gradient-to-b from-[#5B3184] to-[#CE9BFF]',
+  psychic: 'bg-gradient-to-b from-[#E52D2D] to-[#F07F7F]',
+
 }
 
 const borderStylePokemonType = {
   grass: "border-[5px] border-green-500",
   fire: "border-[5px] border-orange-500",
+  water: 'border-[5px] border-blue-500',
+  rock: 'border-[5px] border-[#7E7E7E]',
+  ghost: 'border-[5px] border-[#323569]',
+  ice: 'border-[5px] border-[#6FBEDF]',
+  dark: 'border-[5px] border-[#0D1211]',
+  fairy: 'border-[5px] border-[#971B45]',
+  electric: 'border-[5px] border-[#0C1395]',
+  dragon: 'border-[5px] border-[#478A93]',
+  normal: 'border-[5px] border-[#735259]',
+  fighting: 'border-[5px] border-[#96402A]',
+  bug: 'border-[5px] border-[#62DB60]',
+  steel: 'border-[5px] border-[#5E736C]',
+  ground: 'border-[5px] border-[#654008]',
+  poison: 'border-[5px] border-[#5B3184]',
+  psychic: 'border-[5px] border-[#E52D2D]',
+
 }
 
 export {
